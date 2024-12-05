5a93086164743f4c9862101d56ef328e013d5edd
Bảo Hà <baronn.ha@gmail.com>
2024-12-05 18:42:32 +0700

    ✨ feat: change config footer

--- packages/preset/src/index.ts
+++ packages/preset/src/index.ts
@@ -7,124 +7,124 @@
 
 import { themes as prismThemes } from "prism-react-renderer";
 import type {
-	Preset,
-	LoadContext,
-	PluginConfig,
-	PluginOptions,
+  Preset,
+  LoadContext,
+  PluginConfig,
+  PluginOptions,
 } from "@docusaurus/types";
 import type { Options, ThemeConfig } from "./options";
 
 function makePluginConfig(
-	source: string,
-	options?: PluginOptions
+  source: string,
+  options?: PluginOptions
 ): string | [string, PluginOptions] {
-	if (options) {
-		return [require.resolve(source), options];
-	}
-	return require.resolve(source);
+  if (options) {
+    return [require.resolve(source), options];
+  }
+  return require.resolve(source);
 }
 
 export default function preset(
-	context: LoadContext,
-	opts: Options = {}
+  context: LoadContext,
+  opts: Options = {}
 ): Preset {
-	const { siteConfig } = context;
-	const { themeConfig } = siteConfig;
-	const { algolia } = themeConfig as Partial<ThemeConfig>;
-	const isProd = process.env.NODE_ENV === "production";
-	const {
-		debug,
-		docs,
-		blog,
-		pages,
-		sitemap,
-		theme,
-		googleAnalytics,
-		gtag,
-		googleTagManager,
-		...rest
-	} = opts;
+  const { siteConfig } = context;
+  const { themeConfig } = siteConfig;
+  const { algolia } = themeConfig as Partial<ThemeConfig>;
+  const isProd = process.env.NODE_ENV === "production";
+  const {
+    debug,
+    docs,
+    blog,
+    pages,
+    sitemap,
+    theme,
+    googleAnalytics,
+    gtag,
+    googleTagManager,
+    ...rest
+  } = opts;
 
-	// overrides prism themes
-	themeConfig.prism = {
-		...(themeConfig.prism ?? {}),
-		theme: prismThemes.github,
-		darkTheme: prismThemes.dracula,
-	}
+  // overrides prism themes
+  themeConfig.prism = {
+    ...(themeConfig.prism ?? {}),
+    theme: prismThemes.github,
+    darkTheme: prismThemes.dracula,
+  };
 
-	// overrides footer
-	themeConfig.footer = {
-		...(themeConfig.footer ?? {}),
-		copyright: `Open Source by <a rel="noreferrer" href="https://gorhom.dev/" target="_blank">Mo Gorhom</a>.`,
-		links: [
-			{
-				label: "Github",
-				href: "https://github.com/gorhom",
-			},
-			{
-				label: "X (Twitter)",
-				href: "https://twitter.com/gorhom",
-			},
-		]
-	}
+  // overrides footer
+  themeConfig.footer = {
+    copyright: `Open Source by <a rel="noreferrer" href="https://gorhom.dev/" target="_blank">Mo Gorhom</a>.`,
+    links: [
+      {
+        label: "Github",
+        href: "https://github.com/gorhom",
+      },
+      {
+        label: "X (Twitter)",
+        href: "https://twitter.com/gorhom",
+      },
+    ],
+    ...(themeConfig.footer ?? {}),
+  };
 
-	const themes: PluginConfig[] = [];
-	themes.push(makePluginConfig("@gorhom/docusaurus-theme", theme));
-	if (algolia) {
-		themes.push(require.resolve("@docusaurus/theme-search-algolia"));
-	}
-	if ("gtag" in themeConfig) {
-		throw new Error(
-			'The "gtag" field in themeConfig should now be specified as option for plugin-google-gtag. For preset-classic, simply move themeConfig.gtag to preset options. More information at https://github.com/facebook/docusaurus/pull/5832.'
-		);
-	}
-	if ("googleAnalytics" in themeConfig) {
-		throw new Error(
-			'The "googleAnalytics" field in themeConfig should now be specified as option for plugin-google-analytics. For preset-classic, simply move themeConfig.googleAnalytics to preset options. More information at https://github.com/facebook/docusaurus/pull/5832.'
-		);
-	}
+  const themes: PluginConfig[] = [];
+  themes.push(makePluginConfig("@gorhom/docusaurus-theme", theme));
+  if (algolia) {
+    themes.push(require.resolve("@docusaurus/theme-search-algolia"));
+  }
+  if ("gtag" in themeConfig) {
+    throw new Error(
+      'The "gtag" field in themeConfig should now be specified as option for plugin-google-gtag. For preset-classic, simply move themeConfig.gtag to preset options. More information at https://github.com/facebook/docusaurus/pull/5832.'
+    );
+  }
+  if ("googleAnalytics" in themeConfig) {
+    throw new Error(
+      'The "googleAnalytics" field in themeConfig should now be specified as option for plugin-google-analytics. For preset-classic, simply move themeConfig.googleAnalytics to preset options. More information at https://github.com/facebook/docusaurus/pull/5832.'
+    );
+  }
 
-	const plugins: PluginConfig[] = [];
-	if (docs !== false) {
-		plugins.push(makePluginConfig("@docusaurus/plugin-content-docs", docs));
-	}
-	if (blog !== false) {
-		plugins.push(makePluginConfig("@docusaurus/plugin-content-blog", blog));
-	}
-	if (pages !== false) {
-		plugins.push(makePluginConfig("@docusaurus/plugin-content-pages", pages));
-	}
-	if (googleAnalytics) {
-		plugins.push(
-			makePluginConfig("@docusaurus/plugin-google-analytics", googleAnalytics)
-		);
-	}
-	if (debug || (debug === undefined && !isProd)) {
-		plugins.push(require.resolve("@docusaurus/plugin-debug"));
-	}
-	if (gtag) {
-		plugins.push(makePluginConfig("@docusaurus/plugin-google-gtag", gtag));
-	}
-	if (googleTagManager) {
-		plugins.push(
-			makePluginConfig(
-				"@docusaurus/plugin-google-tag-manager",
-				googleTagManager
-			)
-		);
-	}
-	if (sitemap !== false && (isProd || debug)) {
-		plugins.push(makePluginConfig("@docusaurus/plugin-sitemap", sitemap));
-	}
-	if (Object.keys(rest).length > 0) {
-		throw new Error(
-			`Unrecognized keys ${Object.keys(rest).join(
-				", "
-			)} found in preset-classic configuration. The allowed keys are debug, docs, blog, pages, sitemap, theme, googleAnalytics, gtag, and googleTagManager. Check the documentation: https://docusaurus.io/docs/using-plugins#docusauruspreset-classic for more information on how to configure individual plugins.`
-		);
-	}
+  const plugins: PluginConfig[] = [];
+  if (docs !== false) {
+    plugins.push(makePluginConfig("@docusaurus/plugin-content-docs", docs));
+  }
+  if (blog !== false) {
+    plugins.push(makePluginConfig("@docusaurus/plugin-content-blog", blog));
+  }
+  if (pages !== false) {
+    plugins.push(makePluginConfig("@docusaurus/plugin-content-pages", pages));
+  }
+  if (googleAnalytics) {
+    plugins.push(
+      makePluginConfig("@docusaurus/plugin-google-analytics", googleAnalytics)
+    );
+  }
+  if (debug || (debug === undefined && !isProd)) {
+    plugins.push(require.resolve("@docusaurus/plugin-debug"));
+  }
+  if (gtag) {
+    plugins.push(makePluginConfig("@docusaurus/plugin-google-gtag", gtag));
+  }
+  if (googleTagManager) {
+    plugins.push(
+      makePluginConfig(
+        "@docusaurus/plugin-google-tag-manager",
+        googleTagManager
+      )
+    );
+  }
+  if (sitemap !== false && (isProd || debug)) {
+    plugins.push(makePluginConfig("@docusaurus/plugin-sitemap", sitemap));
+  }
+  if (Object.keys(rest).length > 0) {
+    throw new Error(
+      `Unrecognized keys ${Object.keys(rest).join(
+        ", "
+      )} found in preset-classic configuration. The allowed keys are debug, docs, blog, pages, sitemap, theme, googleAnalytics, gtag, and googleTagManager. Check the documentation: https://docusaurus.io/docs/using-plugins#docusauruspreset-classic for more information on how to configure individual plugins.`
+    );
+  }
 
-	return { themes, plugins };
+  return { themes, plugins };
 }
 
 export type { Options, ThemeConfig };
